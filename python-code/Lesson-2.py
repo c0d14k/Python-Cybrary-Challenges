18f60eb10d302cd9feaa23b71da2ce97b3580efe
Aslam Admani <aslam.admani@hotmail.co.uk>
2017-05-30 15:16:19 +0100

    Update Lesson-2.py

--- python-code/Lesson-2.py
+++ python-code/Lesson-2.py
@@ -1,9 +1,16 @@
-#answers 
-
-print "Hello world"
+print "Hello world"  
 print 'I am codiak, hahaha or they say
 print "I hope you guys benefit from this, I enjoy making these for anyone that learns"
 print "They\'re watching me"
 pr1nt  "Lol - this is a clueeeeeee" + " ooooooo"
 print 'errrrmm what am I talking about? ' + "what is going on"
 print '  Hello World'
+
+
+#1 True
+#2 False
+#3 True
+#4 This is True, however the \ character is unnecessarry 
+#5 False
+#6 True
+#7 True
