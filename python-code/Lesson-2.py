2d27a7029a18b0a389899bc2e19c7ea1c343ddd5
Aslam Admani <aslam.admani@hotmail.co.uk>
2017-05-30 15:14:06 +0100

    Create Lesson-2.py

---
+++ python-code/Lesson-2.py
@@ -0,0 +1,9 @@
+#answers 
+
+print "Hello world"
+print 'I am codiak, hahaha or they say
+print "I hope you guys benefit from this, I enjoy making these for anyone that learns"
+print "They\'re watching me"
+pr1nt  "Lol - this is a clueeeeeee" + " ooooooo"
+print 'errrrmm what am I talking about? ' + "what is going on"
+print '  Hello World'
